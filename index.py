fb36e49060e7b5975b3198b4cfe4a45a113efd6e
João Andrade <joao.andraade25@gmail.com>
2023-01-14 23:04:13 -0300

    rodando comandos por voz

--- index.py
+++ index.py
@@ -1,46 +1,22 @@
 import speech_recognition as sr
-import re
-import pyttsx3 
+import webbrowser
 
-nome = ""
+r = sr.Recognizer()
+mics = sr.Microphone.list_microphone_names()
 
 while(True):
-    mic = sr.Recognizer()
-    with sr.Microphone() as source:
+    with sr.Microphone(device_index=mics.index('Microfone (HyperX SoloCast)')) as source:
+        print("Diga alguma coisa...")
+        audio = r.listen(source)
 
-        engine = pyttsx3.init()
-        engine.setProperty('voice', "com.apple.speech.synthesis.voice.luciana")
-        mic.adjust_for_ambient_noise(source)
+    try:
+        text = r.recognize_google(audio,language='pt-BR')
+        print("Você disse: " + text)
 
-        print("Vamos começar, fale alguma coisa...")
+        if "navegador" in text:
+            webbrowser.open_new_tab('https://www.google.com')
+        elif "YouTube" in text:
+            webbrowser.open_new_tab('https://www.youtube.com')
 
-        audio = mic.listen(source)
-
-        try:
-
-            frase = mic.recognize_google(audio,language='pt-BR')
-
-            if (re.search(r'\b' + "toca no" + r'\b',format(frase))):
-
-                engine.say("Calleri que é gol");
-                engine.runAndWait()
-
-                print("Vamo São Paulo")
-
-
-
-            elif (re.search(r'\b' + "meu nome é " + r'\b',format(frase))):
-
-                t = re.search('meu nome é (.*)',format(frase))
-                nome = t.group(1)
-                print("Seu nome é "+nome)
-                engine.say("Nome falado foi "+nome);
-                engine.runAndWait()
-
-            print("Voce falou: "+frase)
-
-        
-
-        except sr.UnknownValueError:
-
-            print("ops, algo deu errado.")
+    except sr.UnknownValueError:
+        print("Não entendi")
